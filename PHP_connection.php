9520510ba325a8e50fedb2ff77b9a0ce6f7b7f65
dahye <dahye0729@sch.ac.kr>
2020-07-21 10:10:53 +0900

    connect DB

---
+++ PHP_connection.php
@@ -0,0 +1,23 @@
+<?php
+
+$con=mysqli_connect("localhos","root","", "dahye");
+
+if(mysqli_connect_errno($con)){
+    echo "Failed to connect to MySQL : ". mysqli_connect_error();
+}
+
+mysqli_set_charset($con,'utf8');
+
+$res = mysqli_query($con, "select*from memberinfo");
+
+$result = array();
+
+while($row = mysqli_fetch_array($res)){
+    array_push($result, array('name'=>$row[0], 'num'=>$row[1]));
+}
+
+echo json_encode(array("result"=>$result));
+
+mysqli_close($con);
+
+?>
