1580758f9799ae8fc809c09cbd1256b91c9281e1
dahye <dahye0729@sch.ac.kr>
2020-07-22 14:20:28 +0900

    hedgehoginfo 출력

--- PHP_connection.php
+++ PHP_connection.php
@@ -1,19 +1,17 @@
 <?php
-
-$con=mysqli_connect("localhos","root","", "dahye");
+$con=mysqli_connect("localhost","root","","hedgehogmember");
 
 if(mysqli_connect_errno($con)){
-    echo "Failed to connect to MySQL : ". mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-mysqli_set_charset($con,'utf8');
+mysqli_set_charset($con,"utf8");
 
-$res = mysqli_query($con, "select*from memberinfo");
-
+$res=mysqli_query($con,"select*from hedgehoginfo");
 $result = array();
 
-while($row = mysqli_fetch_array($res)){
-    array_push($result, array('name'=>$row[0], 'num'=>$row[1]));
+while($row=mysqli_fetch_array($res)){
+    array_push($result, array('status'=>$row[0], 'name'=>$row[1],'num'=>$row[2]));
 }
 
 echo json_encode(array("result"=>$result));
