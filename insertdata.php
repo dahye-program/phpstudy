727f390ada2652b96407dd59d7f339d611da2f76
dahye <dahye0729@sch.ac.kr>
2020-08-13 14:20:36 +0900

    hedgehogmember db

--- insertdata.php
+++ insertdata.php
@@ -1,18 +1,18 @@
 <?php
-$con=mysqli_connect("localhost","root","","test");
+$con=mysqli_connect("localhost","root","","hedgehogmember");
 
 if(mysqli_connect_errno($con)){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-mysqli_set_charset($con,"utf8");
+mysqli_set_charset($con,"utf8"); // db와 데이터를 주고 받을 때 사용할 기본 문자
 
-$userStatus=$_POST['id'];
+$userStatus=$_POST['status'];
 $userName=$_POST['name'];
 $userNumber=$_POST['number'];
 
 
-$sql="INSERT INTO memberinfo(id, name, number)VALUES('$userStatus','$userName','$userNumber')";
+$sql="INSERT INTO hedgehoginfo(status, name, number)VALUES('$userStatus','$userName','$userNumber')";
 
 if($con->query($sql)==TRUE){
     echo "New record create successfully";
